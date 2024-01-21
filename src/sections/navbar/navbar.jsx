5a86c4ed25f9776f7c0523ee622e66946ace8f33
Shwetaank <spmorey87@gmail.com>
2024-01-21 19:29:28 +0530

    styling Navbar

--- src/sections/navbar/navbar.jsx
+++ src/sections/navbar/navbar.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import Logo from "../../assets/logo.jpg";
+import data from "./data";
+import { IoColorPaletteSharp } from "react-icons/io5";
+import "./navbar.css";
 
 const navbar = () => {
   return (
     <nav>
-      <h2>Navbar</h2>
-    </nav>
-  )
-}
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
+          <img src={Logo} alt="Shwetank Logo" />
+        </a>
+        <ul className="nav__menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
 
-export default navbar
+        <button id="theme__icon">
+          <IoColorPaletteSharp />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default navbar;
