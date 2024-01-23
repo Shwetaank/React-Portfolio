091713d1419727677a2711b4888dd6783d10145b
Shwetaank <spmorey87@gmail.com>
2024-01-23 13:26:40 +0530

    added About page

---
+++ src/sections/about/data.js
@@ -0,0 +1,21 @@
+import { FaAward } from "react-icons/fa";
+import { TbBook } from "react-icons/tb";
+import { BiHappyHeartEyes } from "react-icons/bi";
+
+const data = [
+  {
+    id: 1,
+    icon: <FaAward />,
+    title: "Experience",
+    desc: "5+ months of freelancing working",
+  },
+  { id: 2, icon: <TbBook />, title: "Projects", desc: "15+ Completed" },
+  {
+    id: 3,
+    icon: <BiHappyHeartEyes />,
+    title: "Client's",
+    desc: "5+ happy Clients",
+  },
+];
+
+export default data;
