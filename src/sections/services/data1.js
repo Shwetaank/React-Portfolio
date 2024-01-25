b991382fdd32bef658e3b1b76843160c04c95052
Shwetaank <spmorey87@gmail.com>
2024-01-25 13:52:30 +0530

    tweak  services.css file

--- src/sections/services/data1.js
+++ src/sections/services/data1.js
@@ -3,7 +3,6 @@ import { FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { SiTypescript } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
-import { FaPython } from "react-icons/fa";
 import { MdComputer } from "react-icons/md";
 import { FaReact } from "react-icons/fa";
 import { TbBrandReactNative } from "react-icons/tb";
@@ -15,13 +14,9 @@ import { SiMui } from "react-icons/si";
 import { DiNodejs } from "react-icons/di";
 import { SiExpress } from "react-icons/si";
 import { SiMariadbfoundation } from "react-icons/si";
-import { BiLogoPostgresql } from "react-icons/bi";
 import { SiMongodb } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
-import { IoLogoFirebase } from "react-icons/io5";
 import { SiAdobephotoshop } from "react-icons/si";
-import { SiPostman } from "react-icons/si";
-import { FaLinux } from "react-icons/fa";
 import { FaGitAlt } from "react-icons/fa";
 
 const data1 = [
@@ -46,14 +41,9 @@ const data1 = [
     title: "TypeScript",
   },
   {
-    id: 4,
+    id: 5,
     icon: <FaJava />,
     title: "Java",
-  },
-  {
-    id: 5,
-    icon: <FaPython />,
-    title: "Python",
   },
   {
     id: 6,
@@ -112,41 +102,21 @@ const data1 = [
   },
   {
     id: 17,
-    icon: <BiLogoPostgresql />,
-    title: "Postgresql",
-  },
-  {
-    id: 18,
     icon: <SiMongodb />,
     title: "Mongo DB",
   },
   {
-    id: 19,
+    id: 18,
     icon: <FaAws />,
     title: "AWS",
   },
   {
-    id: 20,
-    icon: <IoLogoFirebase />,
-    title: "Firebase",
-  },
-  {
-    id: 21,
+    id: 19,
     icon: <SiAdobephotoshop />,
     title: "PhotoShop",
   },
   {
-    id: 22,
-    icon: <SiPostman />,
-    title: "Postman",
-  },
-  {
-    id: 23,
-    icon: <FaLinux />,
-    title: "Linux",
-  },
-  {
-    id: "24",
+    id: 20,
     icon: <FaGitAlt />,
     title: "Git",
   },
