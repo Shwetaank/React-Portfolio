16f9347179d5589ecca38b8f4d16252f09b7a570
Shwetaank <spmorey87@gmail.com>
2024-01-27 09:35:41 +0530

    added functanility for Testimonial Section

---
+++ src/sections/testimonials/data.js
@@ -0,0 +1,76 @@
+import Avtar1 from "../../assets/Avtar1.jpg";
+import Avtar2 from "../../assets/Avtar2.jpg";
+import Avtar3 from "../../assets/Avtar3.jpg";
+import Avtar4 from "../../assets/Avtar4.jpg";
+import Avtar5 from "../../assets/Avtar5.jpeg";
+import Avtar6 from "../../assets/Avtar6.jpg";
+import Avtar7 from "../../assets/Avtar7.jpeg";
+
+const data = [
+  {
+    id: 1,
+    quote:
+      "Outstanding service! 🌟 Our website was transformed into a modern, user-friendly platform that exceeded our expectations. 🚀 ",
+    avtar: Avtar1,
+    name: "Rohan Roy",
+    profession: "Gym Owner",
+    Rating: "⭐⭐⭐⭐⭐",
+  },
+  {
+    id: 2,
+    quote:
+      "🚀 The team's expertise, professionalism, and commitment to delivering high-quality results were evident throughout the entire project.",
+    avtar: Avtar2,
+    name: "John Snowdan",
+    profession: "Statup Owner",
+    Rating: "⭐⭐⭐⭐⭐",
+  },
+  {
+    id: 3,
+    quote:
+      "Impressive backend services by Shwetank! 💼 He seamlessly integrated advanced backend solutions into our banking platform, enhancing efficiency and security",
+    avtar: Avtar3,
+    name: "Sass bank",
+    profession: "Pvt Bank",
+    Rating: "⭐⭐⭐⭐",
+  },
+  {
+    id: 4,
+    quote:
+      " 🚗 The user-friendly and visually appealing frontend solutions he implemented have greatly enhanced our online presence.",
+    avtar: Avtar4,
+    name: "Naginx Automobile",
+    profession: "Vechile Manufacture",
+    Rating: "⭐⭐⭐⭐",
+  },
+  {
+    id: 5,
+    quote:
+      "🔒 We are extremely satisfied with the innovative solutions he has delivered. 🚀",
+    avtar: Avtar5,
+    name: "Marcone",
+    profession: "Free Lance Devloper",
+    Rating: "⭐⭐⭐⭐",
+  },
+  {
+    id: 6,
+    quote:
+      "Working with Shwetank on mobile development has been a delightful experience.📱 Shwetank's attention to detail and commitment to user experience have set a new standard for our mobile development initiatives. 👏",
+    avtar: Avtar6,
+    name: "Ronark Saus",
+    profession: "Mobile dev ",
+    Rating: "⭐⭐⭐",
+  },
+  {
+    id: 7,
+    quote:
+      "Exceptional website design services by Shwetank! 🌟 Shwetank's ability to bring out the glamour in every detail is truly impressive. 💫",
+
+    avtar: Avtar7,
+    name: "Monika",
+    profession: "Actress",
+    Rating: "⭐⭐⭐⭐",
+  },
+];
+
+export default data;
--- src/sections/testimonials/testimonails.jsx
+++ src/sections/testimonials/testimonails.jsx
@@ -1,12 +1,44 @@
+// testimonails.jsx
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css/autoplay";
+import Testimonial from "./Testimonial";
+import testimonails from "./data";
 import "./testimonails.css";
 
-const testimonails = () => {
+const TestimonialList = () => {
   return (
     <section id="testimonials">
-      <h2> Testimonial </h2>
+      <h2>What My Clients Say</h2>
+      <p>
+        Glowing client testimonials! 🌟 Our work surpasses expectations, turning
+        websites into modern marvels.
+      </p>
+      <div className="container">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            601: { slidesPerView: 1 },
+            1025: { slidesPerView: 2 },
+          }}
+          pagination={{ clickable: true }}
+          modules={[Pagination, Autoplay]}
+          autoplay={true}
+          className="mySwiper"
+        >
+          {testimonails.map((testimonial) => (
+            <SwiperSlide key={testimonial.id}>
+              <Testimonial testimonial={testimonial} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
 
-export default testimonails;
+export default TestimonialList;
