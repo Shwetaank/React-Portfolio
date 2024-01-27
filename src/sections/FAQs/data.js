0dc74effe8974d0e70182b6f9c3ec372eb881066
Shwetaank <spmorey87@gmail.com>
2024-01-27 14:12:46 +0530

    added functanility to the Faqs Section

---
+++ src/sections/FAQs/data.js
@@ -0,0 +1,53 @@
+const data = [
+  {
+    id: 1,
+    Question:
+      "🚀 What projects have you worked on, and what challenges did you face during their development?",
+    Answer:
+      "As a fresher, I've completed impactful projects during my training. Challenges often revolved around mastering asynchronous operations and ensuring seamless communication between frontend and backend.",
+  },
+  {
+    id: 2,
+    Question:
+      "🔐 How do you approach securing application, especially in terms of user authentication and authorization?",
+    Answer:
+      "Security is a top priority. I implement secure authentication using JWT tokens, validate user inputs rigorously, and guard against common vulnerabilities like XSS and CSRF to ensure a robust and secure application.",
+  },
+  {
+    id: 3,
+    Question:
+      "⚙️ How do you optimize the performance of a MERN stack application, and what strategies do you employ for faster rendering and responsiveness?",
+    Answer:
+      "Performance optimization is crucial. I focus on code splitting, lazy loading, and use tools like React DevTools for profiling. Additionally, I employ techniques like image optimization and asynchronous loading for faster rendering and improved responsiveness.",
+  },
+  {
+    id: 4,
+    Question:
+      "💼 Can you describe your experience with writing smart contracts, and which programming languages do you use for developing on blockchain platforms?",
+    Answer:
+      "I have hands-on experience in writing smart contracts using languages such as Solidity for Ethereum. My projects involved creating and deploying smart contracts to implement various decentralized functionalities and business logic.",
+  },
+  {
+    id: 5,
+    Question:
+      "🌐 Could you explain your understanding of decentralized applications (DApps) and your experience in building or contributing to them on blockchain platforms?",
+    Answer:
+      "I have practical experience in developing decentralized applications (DApps) on blockchain platforms. This involves creating frontend interfaces that interact seamlessly with smart contracts deployed on the blockchain, providing users with a decentralized and secure experience.",
+  },
+  {
+    id: 6,
+    Question:
+      "📲 Can you describe your experience with integrating third-party libraries and APIs into React Native applications?",
+    Answer:
+      "I have successfully integrated various third-party libraries and APIs into React Native projects. This includes incorporating features like maps, authentication, and push notifications, enhancing the functionality of the mobile applications.",
+  },
+  {
+    id: 7,
+    Question:
+      "🧪 Could you explain your understanding of mobile app testing, and what testing tools or methodologies do you use for React Native applications?",
+    Answer:
+      "Testing is integral to ensuring a reliable mobile app. I conduct unit testing using Jest for components and functions, and I use tools like Detox or Appium for end-to-end testing. This ensures that the app functions correctly across different devices and scenarios.",
+  },
+];
+
+export default data;
