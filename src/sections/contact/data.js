cf5815c5f4d38767fce78dbabe7af265b3ed077d
Shwetaank <spmorey87@gmail.com>
2024-01-27 17:28:28 +0530

    design contact me section

---
+++ src/sections/contact/data.js
@@ -0,0 +1,23 @@
+import { IoIosMail } from "react-icons/io";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+
+const data = [
+  {
+    id: 1,
+    icon: <IoIosMail />,
+    link: "mailto:spmorey87@gmail.com",
+  },
+  {
+    id: 2,
+    icon: <FaFacebookMessenger />,
+    link: "https://www.facebook.com/messages/t/100001121937194",
+  },
+  {
+    id: 3,
+    icon: <FaWhatsapp />,
+    link: "https://wa.me/919421317759",
+  },
+];
+
+export default data;
