f86f0da78550ec0280930219bf4ecb7ce0cdf79d
Shwetaank <spmorey87@gmail.com>
2024-01-31 15:49:49 +0530

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
         <FAQs />
         <Contact />
         <Footer />
-        <FloatingNav />
+        {/* <FloatingNav /> */}
       </main>
     </>
   );
--- src/sections/footer/footer.jsx
+++ src/sections/footer/footer.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import './footer.css'
+import { FaHeart } from "react-icons/fa";
+import { links, socials } from "./data";
+import "./footer.css";
 
-const footer = () => {
+const Footer = () => {
   return (
     <footer>
-      <h2>Footer</h2>
+      <div className="container footer__contyainer">
+        <ul className="nav__menu">
+          {links.map((fLink) => (
+            <li key={fLink.id}>
+              <a href={fLink.link}> {fLink.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="footer__socials">
+          {socials.map((socail) => (
+            <a
+              key={socail.id}
+              href={socail.link} // Fix here, it should be socail.link
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {socail.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <small>
+          2024 Sin_Greed. &copy; All Rights Reserved. <br />
+          Infused with <FaHeart />.
+        </small>
+      </div>
     </footer>
   );
 };
 
-export default footer;
+export default Footer;
