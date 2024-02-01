5ab5a847dd6dff331f32ca4109fc8f8a968529ba
Shwetaank <spmorey87@gmail.com>
2024-02-02 01:01:09 +0530

    added functanility for floating navbar for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import Navbar from "./sections/navbar/navbar";
 import Header from "./sections/header/header";
 import About from "./sections/about/about";
@@ -16,9 +16,44 @@ import { useThemeContext } from "./context/theme-context";
 
 const App = () => {
   const { themeState } = useThemeContext();
+
+  const mainRef = useRef();
+  const [showFloatingNav, setShowFloatingNav] = useState(true);
+  const [siteYPosition, setSiteYPosition] = useState(0);
+
+  // Memoize the function using useCallback
+  const floatingNavToggleHandler = useCallback(() => {
+    // Move the definition of showFloatingNavHandler inside the useCallback callback
+    const showFloatingNavHandler = () => {
+      setShowFloatingNav(true);
+    };
+
+    // check if we scrolled up or down at least 20px
+    if (
+      siteYPosition < mainRef?.current?.getBoundingClientRect().y - 20 ||
+      siteYPosition > mainRef?.current?.getBoundingClientRect().y + 20
+    ) {
+      showFloatingNavHandler();
+    } else {
+      setShowFloatingNav(false); // directly set it to false without calling the function
+    }
+
+    setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
+  }, [siteYPosition]);
+
+  useEffect(() => {
+    const checkYPosition = setInterval(floatingNavToggleHandler, 3000);
+
+    // Cleanup function
+    return () => clearInterval(checkYPosition);
+  }, [siteYPosition, floatingNavToggleHandler]);
+
   return (
     <>
-      <main className={`${themeState.primary} ${themeState.background}`}>
+      <main
+        className={`${themeState.primary} ${themeState.background}`}
+        ref={mainRef}
+      >
         <Navbar />
         <Header />
         <About />
@@ -31,7 +66,7 @@ const App = () => {
         <Contact />
         <Footer />
         <Theme />
-        {/* <FloatingNav /> */}
+        {showFloatingNav && <FloatingNav />}
       </main>
     </>
   );
--- src/sections/floating-nav/floatingNav.jsx
+++ src/sections/floating-nav/floatingNav.jsx
@@ -1,8 +1,33 @@
-import React from "react";
 import "./floatingNav.css";
-
+import Scrollspy from "react-scrollspy";
+import Nav from "./Nav";
+import data from "./data";
 const floatingNav = () => {
-  return <section id="floating_Nav"></section>;
+  return (
+    <>
+      <ul id="floating__Nav">
+        <Scrollspy
+          offset={-400}
+          className="scrollspy"
+          items={[
+            "header",
+            "about",
+            "Certificates",
+            "services",
+            "blog",
+            "portfolio",
+            "contact",
+          ]}
+          currentClassName="active"
+        >
+          {data.map((item) => (
+            <Nav key={item.id} item={item} />
+          ))}
+        </Scrollspy>
+      </ul>
+      ;
+    </>
+  );
 };
 
 export default floatingNav;
