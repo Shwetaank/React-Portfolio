faf6626051c60950a42436c4d464af5dad8651b4
Shwetaank <spmorey87@gmail.com>
2024-02-09 12:47:30 +0530

    redeisgn FAQs section

--- src/sections/certificates/data1.js
+++ src/sections/certificates/data1.js
@@ -4,50 +4,59 @@ import Img3 from "../../assets/Cert/HTML & CSS.png";
 import Img4 from "../../assets/Cert/Node.js.png";
 import Img5 from "../../assets/Cert/JavaScript Programming.png";
 import Img6 from "../../assets/Cert/60 HTML CSS JS projects - HTML5, CSS3 and vanilla JavaScript.jpg";
+import Img7 from "../../assets/Cert/JavaScript (Basic) Certificate.png";
 
 const data1 = [
   {
     id: 1,
     Img: Img1,
     title: "Introduction to Computer Programming",
-    desc: "In this Computer Programming course, I delved into the world of coding with JavaScript and Java. 🚀 From mastering syntax to honing problem-solving skills, I've cultivated the ability to write efficient code, fostering a creative mindset for innovation in the tech world. 💻✨",
+    desc: "🚀 Explored coding with JavaScript and Java, mastering syntax and problem-solving. Cultivated a creative mindset for tech innovation. Developed skills for efficient code writing and innovative thinking in the tech world.",
     link: "https://certificates.almabetter.com/en/verify/12912738111141",
   },
   {
     id: 2,
     Img: Img2,
     title: "Design & Analysis of Algorithms",
-    desc: "In the Design & Analysis of Algorithms course, I navigated the intricacies of algorithmic design and efficiency evaluation. 🌐 From conceptualizing algorithms to analyzing their performance, I've honed skills to craft optimized solutions, fostering a strategic mindset for innovation in the tech world. 💡📊",
+    desc: "💡 Navigated algorithmic design intricacies, honing skills to craft optimized solutions. Fostered a strategic mindset for tech innovation. Analyzed performance and conceptualized algorithms for efficient problem-solving.",
     link: "https://certificates.almabetter.com/en/verify/22045576347910",
   },
   {
     id: 3,
     Img: Img3,
     title: "HTML & CSS",
-    desc: "Mastered the art of crafting visually appealing websites by combining HTML for structure and CSS for style. 🎨💻 Ready to shape seamless digital experiences! 🚀🌐",
+    desc: "🎨 Mastered crafting visually appealing websites with HTML and CSS. Developed expertise in structuring and styling web content. Ready to create seamless digital experiences with a blend of creativity and technical skills.",
     link: "https://certificates.almabetter.com/en/verify/34314295067940",
   },
   {
     id: 4,
     Img: Img4,
     title: "Node.js",
-    desc: "Dived into server-side JavaScript, mastering the art of building scalable and efficient web applications. ⚙️💻 From handling requests to managing data, equipped to create dynamic and high-performance server systems. 🌐🚀",
+    desc: "⚙️ Dived into server-side JavaScript, mastering building scalable web applications. Equipped to create dynamic server systems. Managed data and requests efficiently for high-performance web applications.",
     link: "https://certificates.almabetter.com/en/verify/20123175383087",
   },
   {
     id: 5,
     Img: Img5,
     title: "JavaScript Programming",
-    desc: "Embraced the power of JavaScript, mastering its syntax and unleashing its potential for dynamic web development. 💻🚀 Equipped to create engaging and responsive web applications. 🌐✨",
+    desc: "💻 Embraced JavaScript's power, mastering syntax for dynamic web development. Equipped to create engaging web applications. Leveraged JavaScript's potential for interactive and responsive web design, fostering user engagement and satisfaction.",
     link: "https://certificates.almabetter.com/en/verify/33056971811545",
   },
   {
     id: 6,
     Img: Img6,
     title: "60 HTML CSS JS projects - HTML5, CSS3 and vanilla JavaScript",
-    desc: "Hands-on mastery of web development using HTML5, CSS3, and Vanilla JavaScript. 🚀💻 Ready to craft diverse and interactive web applications! 🌐✨",
+    desc: "🚀💻 Hands-on mastery of web development with HTML5, CSS3, and Vanilla JavaScript. Ready to craft diverse web applications. Built a portfolio of projects demonstrating proficiency in front-end web technologies.",
     link: "https://www.udemy.com/certificate/UC-d850b362-12f8-4d1b-bb57-c25ad8b7b304/",
   },
+  {
+    id: 7,
+    Img: Img7,
+    title: "JavaScript (Basic) Certificate",
+    desc: "📜 Verify proficiency in basic JavaScript concepts and syntax. Signifies understanding of fundamental programming principles. Able to write and comprehend basic JavaScript code, laying a solid foundation for further learning and development.",
+    link: "https://www.hackerrank.com/certificates/b5994e9eb865"
+  }
 ];
 
 export default data1;
+
