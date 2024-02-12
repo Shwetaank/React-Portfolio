37c73f9dc4b5106f313d09b54669e8de9368d7df
Shwetaank <spmorey87@gmail.com>
2024-02-12 12:31:46 +0530

    redisgn Certification Section

--- src/sections/certificates/data1.js
+++ src/sections/certificates/data1.js
@@ -1,11 +1,7 @@
 import Img1 from "../../assets/Cert/Introduction to Computer Programming.png";
 import Img2 from "../../assets/Cert/Design & Analysis of Algorithms.png";
-import Img3 from "../../assets/Cert/HTML & CSS.png";
-import Img4 from "../../assets/Cert/Node.js.png";
-import Img5 from "../../assets/Cert/JavaScript Programming.png";
-import Img6 from "../../assets/Cert/60 HTML CSS JS projects - HTML5, CSS3 and vanilla JavaScript.jpg";
-import Img7 from "../../assets/Cert/JavaScript (Basic) Certificate.png";
-import Img8 from "../../assets/Cert/JavaScript (Intermediate) Certificate.png";
+import Img3 from "../../assets/Cert/JavaScript Programming.png";
+import Img4 from "../../assets/Cert/JavaScript (Intermediate) Certificate.png";
 
 const data1 = [
   {
@@ -25,41 +21,13 @@ const data1 = [
   {
     id: 3,
     Img: Img3,
-    title: "HTML & CSS",
-    desc: "🎨 Mastered crafting visually appealing websites with HTML and CSS. Developed expertise in structuring and styling web content. Ready to create seamless digital experiences with a blend of creativity and technical skills.",
-    link: "https://certificates.almabetter.com/en/verify/34314295067940",
-  },
-  {
-    id: 4,
-    Img: Img4,
-    title: "Node.js",
-    desc: "⚙️ Dived into server-side JavaScript, mastering building scalable web applications. Equipped to create dynamic server systems. Managed data and requests efficiently for high-performance web applications.",
-    link: "https://certificates.almabetter.com/en/verify/20123175383087",
-  },
-  {
-    id: 5,
-    Img: Img5,
     title: "JavaScript Programming",
     desc: "💻 Embraced JavaScript's power, mastering syntax for dynamic web development. Equipped to create engaging web applications. Leveraged JavaScript's potential for interactive and responsive web design, fostering user engagement and satisfaction.",
     link: "https://certificates.almabetter.com/en/verify/33056971811545",
   },
   {
-    id: 6,
-    Img: Img6,
-    title: "60 HTML CSS JS projects - HTML5, CSS3 and vanilla JavaScript",
-    desc: "🚀💻 Hands-on mastery of web development with HTML5, CSS3, and Vanilla JavaScript. Ready to craft diverse web applications. Built a portfolio of projects demonstrating proficiency in front-end web technologies.",
-    link: "https://www.udemy.com/certificate/UC-d850b362-12f8-4d1b-bb57-c25ad8b7b304/",
-  },
-  {
-    id: 7,
-    Img: Img7,
-    title: "JavaScript (Basic) Certificate",
-    desc: "📜 Verify proficiency in basic JavaScript concepts and syntax. Signifies understanding of fundamental programming principles. Able to write and comprehend basic JavaScript code, laying a solid foundation for further learning and development.",
-    link: "https://www.hackerrank.com/certificates/b5994e9eb865",
-  },
-  {
-    id: 8,
-    Img: Img8,
+    id: 4,
+    Img: Img4,
     title: "JavaScript (Intermediate) Certificate",
     desc: "🎓 Demonstrates proficiency in intermediate-level JavaScript concepts. Signifies mastery of advanced topics such as Design Patterns, Memory Management, Concurrency Model, and Event Loops, among others. Validates ability to solve complex problems and develop efficient JavaScript applications.",
     link: "https://www.hackerrank.com/certificates/776dc63de26e",
